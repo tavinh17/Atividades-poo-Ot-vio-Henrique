fe7d9bfcb3376c4ceb0cb0e5f8527e9929f25b12
Otavio Henrique <otaviohhenrique17@gmail.com>
2026-06-17 17:02:33 -0300

    Update lab8_q3.py

--- lab8_q3.py
+++ lab8_q3.py
@@ -3,4 +3,25 @@ class Carteira:
         self.__moeda = moeda
         self.__saldo = saldo
 
-    def converter_yuan(self, USD, CNY, BRL)
+    def converter_yuan(self, valor_yuan):
+        if self.moeda == "USD":
+            return valor_yuan * 0.14
+        elif self.moeda == "BRL":
+            return valor_yuan * 0.85
+        elif self.moeda == "CNY":
+            return valor_yuan
+        else:
+            print("Erro")
+
+def __add__(self, valor_yuan):
+    valor_convertido = self.converter_yuan(valor_yuan)
+    return self.saldo + valor_convertido
+
+def __sub__(self, valor_yuan):
+    valor_convertido = self.converter_yuan(valor_yuan)
+    return self.saldo - valor_convertido
+
+carteira_usd = Carteira("USD", 10.0)
+# 50 CNY = 7.0 USD (usando taxa 1 CNY = 0.14 USD) 
+print('Soma de carteira USD + 50 yuan = ', carteira_usd + 50.0) # 17.0
+                       
