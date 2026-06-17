064eef2ef592625ae13fd7b08acc28f3bafb09d4
Otavio Henrique <otaviohhenrique17@gmail.com>
2026-06-17 17:16:16 -0300

    Update lab8_q4.py

--- lab8_q4.py
+++ lab8_q4.py
@@ -1,1 +1,10 @@
+class Pessoa:
+  def __init__(self, nome, altura):
+      self.nome = nome
+      self.altura = altura
 
+  def __str__(self):
+    return f"Nome: {self.nome}, Altura: {self.altura} m"
+    
+def __gt__(self, other):
+  
