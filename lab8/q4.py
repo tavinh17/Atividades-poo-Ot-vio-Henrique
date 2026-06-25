5488e1c17b61a397bbd309b8833e0e972794abb7
Otavio Henrique <otaviohhenrique17@gmail.com>
2026-06-25 15:01:10 -0300

    Update q4.py

--- lab8/q4.py
+++ lab8/q4.py
@@ -1,10 +1,23 @@
-class Pessoa:
-  def __init__(self, nome, altura):
-      self.nome = nome
-      self.altura = altura
-
+classe Pessoa:
+  def __init__ (self, nome: str, altura: float):
+    self.nome = nome
+    self.altura = float(altura)
   def __str__(self):
-    return f"Nome: {self.nome}, Altura: {self.altura} m"
+    return f"Nome: {self.nome} | Altura: {self.altura}"
+  def __gt__(self, other):
+    retorne self.altura > other.altura
+  def __lt__(self, other):
+    retornar self.altura <outro.altura
     
-def __gt__(self, other):
-  
+lst = []
+D = int(input("Quantas pessoas estão cadastradas?"))
+para i em range(D):
+  nome = input("Qual o nome da pessoa?")
+  h = float("Qual a altura da pessoa?")
+  p = Pessoa(nome, h)
+  lista.append(p)
+imprimir('MAIOR:', max(lst))
+imprimir('MENOR:', min(lst))
+para pessoa em sorted(lst):
+  imprimir(pessoa)
+
