f4dcba238310a163be2bda515eb10a4855e84824
Otavio Henrique <otaviohhenrique17@gmail.com>
2026-07-07 09:52:49 -0300

    Update Scripts.py

--- laboratório9/Scripts.py
+++ laboratório9/Scripts.py
@@ -19,7 +19,27 @@ nome = input("Digite seu nome: ")
 escada = ""
 for letra in nome:
     escada = escada + letra
-    print(escada)
+    print(escada)cs
 
 #questão 04 
+
+if len(num) == 9:
+    if num[0] == "9":  
+        print(f"Número Completo: {num[:5]}-{num[5:]}")
+    else:
+        print("Número Incorreto!")
+elif len(num) == 8:
+    print(f"Número Completo: 9{num[:4]}-{num[4:]}")
+else:
+    print("Número Errado!")
+
+#questão 5
+
+quantidade = 0
+for i, letra in enumerate(txt):
+    if letra in "AEIOUaeiou":
+        ind.append(i)
+        qnt += 1
+print(f"Índices: {", ".join(ind)}")
+print("Total:",quantidade)
     
